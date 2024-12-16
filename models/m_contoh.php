8184be08a30b0cec3127636c26436434f9d1c34d
ilman2000 <ilmanwaterzon@gmail.com>
2024-12-15 17:41:02 -0800

    edit,tampil,hapus

--- models/m_contoh.php
+++ models/m_contoh.php
@@ -1,5 +1,22 @@
 <?php 
 
-class m_contoh{}
+class m_contoh{
+
+
+public function tampil() {
+	<!--fungsi buat tampil-->
+}
+
+public function edit() {
+	<!--fungsi buat edit-->
+}
+
+
+public function hapus() {
+	<!--fungsi buat hapus-->
+}
+
+
+}
 
  ?>
